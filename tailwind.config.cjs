b305a10d5037f0ac767df43a50f029fc8d5a3cc9
Adham-Niazy <adhamniazy@gmail.com>
2022-10-06 05:17:52 +0200

    Setup tailwind custom config & colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background': '#16161a',
+        'headline': '#fffffe',
+        'copy': '#94a1b2',
+        'accent': '#7f5af0',
+        'accent-text': '#fffffe',
+      }
+    },
   },
   plugins: [],
 }
