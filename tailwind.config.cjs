ffc6602fc5aaf6f9f040a36068c6386e0e644ba2
Adham-Niazy <adhamniazy@gmail.com>
2022-10-07 18:25:35 +0200

    adding more custom config to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,12 +5,18 @@ module.exports = {
     extend: {
       colors: {
         'background': '#ffffff',
+        'gray-1-background': '#f3f3f4',
         'headline': '#0d0c22',
         'copy': '#6e6d7a',
         'accent': '#ea4c89',
+        'pale-shadow-accent': '#FDEDF3',
         'accent-text': '#ffffff',
       }
     },
+    fontFamily: {
+      inter: ['Inter', 'sans-serif'],
+      fira: ['Fira Code', 'sans-serif'],
+    }
   },
   plugins: [],
 }
