95625493c1d433f47bd13775f34c39b90fae7027
Adham-Niazy <adhamniazy@gmail.com>
2022-10-09 10:41:54 +0200

    Making Navbar more reusable

--- src/components/Navigation/Navigation.svelte
+++ src/components/Navigation/Navigation.svelte
@@ -2,6 +2,8 @@
 	import Search from '../Search/Search.svelte';
 
 	export let withSearch = false;
+	export let title = '';
+	export let counter = 0;
 </script>
 
 <nav>
@@ -9,8 +11,8 @@
 		class="py-6 border-b-2 border-gray-200/75 flex sm:justify-between sm:items-end sm:flex-row flex-col"
 	>
 		<li class="text-xl sm:mb-0 mb-3">
-			<span class="text-headline font-bold mr-1">Collections</span>
-			<span class="text-copy/50">0</span>
+			<span class="text-headline font-bold mr-1">{title}</span>
+			<span class="text-copy/50">{counter}</span>
 		</li>
 
 		{#if withSearch}
