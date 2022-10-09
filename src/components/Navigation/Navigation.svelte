0e296ab73348d9d0745e54eeefbca7eb6d978285
Adham-Niazy <adhamniazy@gmail.com>
2022-10-09 10:27:20 +0200

    Making navigationbar decoupled from search

--- src/components/Navigation/Navigation.svelte
+++ src/components/Navigation/Navigation.svelte
@@ -1,16 +1,22 @@
 <script>
-	import Search from "../Search/Search.svelte";
+	import Search from '../Search/Search.svelte';
 
+	export let withSearch = false;
 </script>
+
 <nav>
-	<ul class="py-6 border-b-2 border-gray-200/75 flex sm:justify-between sm:items-end sm:flex-row flex-col">
+	<ul
+		class="py-6 border-b-2 border-gray-200/75 flex sm:justify-between sm:items-end sm:flex-row flex-col"
+	>
 		<li class="text-xl sm:mb-0 mb-3">
 			<span class="text-headline font-bold mr-1">Collections</span>
 			<span class="text-copy/50">0</span>
 		</li>
 
-    <li>
-      <Search extraStyling="p-3" placeholder="Search Collections.."/>
-    </li>
+		{#if withSearch}
+			<li>
+				<Search extraStyling="p-3" placeholder="Search Collections.." />
+			</li>
+		{/if}
 	</ul>
 </nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 <div>
 	<Intro />
 
-	<Navigation />
+	<Navigation withSearch={true}/>
 
 	<section class="grid gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 py-8">
 		{#each [1, 2, 3, 4] as collection (collection)}
