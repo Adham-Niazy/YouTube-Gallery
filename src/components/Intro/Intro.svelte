ac740155afa13e2ccd7695412deca5cf43e7cfe0
Adham-Niazy <adhamniazy@gmail.com>
2022-10-09 07:26:04 +0200

    Validating Input for accepting only a valid Youtube Link

--- src/components/Intro/Intro.svelte
+++ src/components/Intro/Intro.svelte
@@ -1,3 +1,32 @@
+<script>
+	let clicked = false;
+	let searchField = '';
+	let error = '';
+
+	function validate() {
+		console.log('Called');
+		if (!clicked) return;
+		if (!searchField.includes('youtube.com/watch?v=')) {
+			error = 'Invalid YouTube Link!';
+			return;
+		}
+		const searchParams = new URLSearchParams(searchField.split('?')[1]);
+		const videoID = searchParams.get('v');
+		console.log(/[a-zA-Z0-9_-]{11}/.test(searchParams.get('v')));
+		console.log(searchParams.has('v'));
+		if (!searchParams.has('v') || !/[a-zA-Z0-9_-]{11}/.test(searchParams.get('v'))) {
+			error = 'Not a YouTube Video Link!';
+			return;
+		}
+		error = '';
+	}
+
+	function onSearch(e) {
+		searchField = e.target.value;
+		validate();
+	}
+</script>
+
 <section class="grid grid-cols-1 md:grid-cols-2 items-center gap-10 py-16">
 	<div>
 		<h1 class="text-headline lg:text-6xl md:text-5xl text-4xl font-bold mb-3">
@@ -16,9 +45,19 @@
 			name="videoId"
 			id="videoId"
 			placeholder="www.youtube.com/watch?v=SR1qcN00cGY"
+			on:input={onSearch}
 		/>
+
+		{#if error !== ''}
+			<div class="text-red-500 mb-3">{error}</div>
+		{/if}
+
 		<button
 			class="bg-accent rounded-lg text-accent-text py-3 px-4 hover:opacity-70 transition font-bold md:w-1/2 w-full font-fira"
+			on:click|preventDefault={() => {
+				clicked = true;
+				validate();
+			}}
 		>
 			Save The Video!
 		</button>
